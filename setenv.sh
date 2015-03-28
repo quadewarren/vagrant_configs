4ee8160a9a185a7477ba2a2debaa371027ec39c3
Kyle Haley <kylephaley@gmail.com>
2015-03-27 22:21:56 -0500

    Added catalina-jmx-remote.jar, updated setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -5,8 +5,4 @@
 #                  -Dcom.sun.management.jmxremote.authenticate=false \
 #                  -Djava.rmi.server.hostname=192.168.33.10"
 
-export CATALINA_OPTS="-Dcom.sun.management.jmxremote=true \
-                  -Dcom.sun.management.jmxremote.port=1099 \
-                  -Dcom.sun.management.jmxremote.ssl=false \
-                  -Dcom.sun.management.jmxremote.authenticate=false \
-                  -Djava.rmi.server.hostname=192.168.33.10"
+export CATALINA_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=192.168.33.10"
