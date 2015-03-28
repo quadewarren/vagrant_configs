126d6defa831d93211caddf8dc7f0504497a710d
Kyle Haley <kylephaley@gmail.com>
2015-03-27 22:36:44 -0500

    Updated boostrap, setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,4 @@
 
-#export JAVA_OPTS="-Dcom.sun.management.jmxremote=true \
-#                  -Dcom.sun.management.jmxremote.port=1099 \
-#                  -Dcom.sun.management.jmxremote.ssl=false \
-#                  -Dcom.sun.management.jmxremote.authenticate=false \
-#                  -Djava.rmi.server.hostname=192.168.33.10"
+export JAVA_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=192.168.33.10"
 
 export CATALINA_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=192.168.33.10"
